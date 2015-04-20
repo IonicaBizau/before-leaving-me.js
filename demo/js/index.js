edc9dc7751cafaa03cd0be07f9ea53aa9592e027
Ionică Bizău <bizauionica@yahoo.com>
2015-04-20 08:29:52 +0300

    More handlers

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,9 +1,16 @@
 window.addEventListener("load", function () {
+    var boxContainerEl = document.querySelector(".box-container");
     blm("Bye bye!");
     blm(function (e) {
         return "Hi";
     });
     blm.prepare(function (e) {
-        document.querySelector(".box").classList.add("active");
+        boxContainerEl.classList.add("active");
+    });
+
+    window.addEventListener("keyup", function (e) {
+        if (e.which === 27) {
+            boxContainerEl.classList.remove("active");
+        }
     });
 });
