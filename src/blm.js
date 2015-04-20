dc3ebf62ee3cae07886f85784e86006db0062931
Ionică Bizău <bizauionica@yahoo.com>
2015-04-20 08:29:27 +0300

    Removed repetitived vode

--- src/blm.js
+++ src/blm.js
@@ -47,76 +47,82 @@
       , lastMouseY = null
       ;
 
+    function handlerPrepares(evName) {
+        return function (e) {
+
+            switch (evName) {
+                case "mouseleave":
+                    if (e.relatedTarget || e.toElement) {
+                        return;
+                    }
+
+                    if (e.clientY <= 0) {
+                        lastMouseY = 0;
+                        lastMouseX = e.clientX;
+                    }
+                    break;
+                case "mousemove":
+                    if (timestamp === null) {
+                        timestamp = Date.now();
+                        lastMouseX = e.screenX;
+                        lastMouseY = e.screenY;
+                        return;
+                    }
+
+                    var now = Date.now()
+                      , dt =  now - timestamp
+                      , dx = e.screenX - lastMouseX
+                      , dy = e.screenY - lastMouseY
+                      , speedX = Math.round(dx / dt * 100)
+                      , speedY = Math.round(dy / dt * 100)
+                      ;
+
+                    if (dt === 0) {
+                        speedX = Infinity;
+                        speedY = Infinity;
+                    }
+
+                    timestamp = now;
+                    lastMouseX = e.screenX;
+                    lastMouseY = e.screenY;
+                    break;
+            }
+
+            // Check the prepares
+            _prepares.forEach(function (c) {
+                if (c.ignore
+                || (evName === "mouseleave" && c.ignore_leave)
+                || (evName === "blur" && c.ignore_leave)
+                || (evName === "mousemove" && (c.ignore_move
+                    || lastMouseY > c.min_y
+                    || speedY >= 0
+                    || c.min_speed < speedY)
+                    )
+                ) { return; }
+
+                c.func.call(c, new Cursor(
+                    lastMouseX, lastMouseY
+                  , speedX, speedY
+                  , dx, dy
+                ));
+
+                c.ignore = true;
+                c.setTimeout = setTimeout(function () {
+                    c.ignore = false;
+                    timestamp = null;
+                }, c.ignore_timeout);
+            });
+        };
+    }
+
     // Catch the mouseleave
-    document.addEventListener("mouseleave", function (e) {
+    document.addEventListener("mouseleave", handlerPrepares("mouseleave"));
 
-        if (e.clientY <= 0) {
-            lastMouseY = 0;
-            lastMouseX = e.clientX;
-        }
-
-        // Check the prepares
-        _prepares.forEach(function (c) {
-            if (c.ignore || c.ignore_leave) { return; }
-            c.func.call(c, new Cursor(
-                lastMouseX, lastMouseY
-              , null, null
-              , null, null
-            ));
-            c.ignore = true;
-            c.setTimeout = setTimeout(function () {
-                c.ignore = false;
-                timestamp = null;
-            }, c.ignore_timeout);
-        });
-    });
+    // On blur
+    root.addEventListener("blur", handlerPrepares("blur"));
 
     // Mousemove
-    document.addEventListener("mousemove", function(e) {
-
-        if (timestamp === null) {
-            timestamp = Date.now();
-            lastMouseX = e.screenX;
-            lastMouseY = e.screenY;
-            return;
-        }
-
-        var now = Date.now()
-          , dt =  now - timestamp
-          , dx = e.screenX - lastMouseX
-          , dy = e.screenY - lastMouseY
-          , speedX = Math.round(dx / dt * 100)
-          , speedY = Math.round(dy / dt * 100)
-          ;
-
-        if (dt === 0) {
-            speedX = Infinity;
-            speedY = Infinity;
-        }
-
-        timestamp = now;
-        lastMouseX = e.screenX;
-        lastMouseY = e.screenY;
-
-        // Check the prepares
-        _prepares.forEach(function (c) {
-            if (lastMouseY > c.min_y
-            || speedY >= 0
-            || c.min_speed < speedY
-            || c.ignore
-            || c.ignore_move) { return; }
-            c.func.call(c, new Cursor(
-                lastMouseX, lastMouseY
-              , speedX, speedY
-              , dx, dy
-            ));
-            c.ignore = true;
-            c.setTimeout = setTimeout(function () {
-                c.ignore = false;
-                timestamp = null;
-            }, c.ignore_timeout);
-        });
-    });
+    document.addEventListener("mousemove", handlerPrepares("mousemove"));
 
     // Listen for the beforeunload event
     root.onbeforeunload =function (e) {
